346d962f3e732bdb05133b7c7dc6e205c4737999
Yosef Sherif <yasherif92@gmail.com>
2025-11-06 10:45:33 +0200

    third push

--- frontend/src/app/[lng]/academics/page.tsx
+++
@@ -1,9 +0,0 @@
-import AcademicsContent from "@/components/sections/AcademicsContent";
-
-export default async function AcademicsPage({ lng }: { lng: string }) {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50 ">
-      <AcademicsContent lng={lng} />
-    </div>
-  );
-}
